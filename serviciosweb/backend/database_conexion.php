e2c355e0f5461eceecb7f25e3be9191fe3aac7da
PacManGod <69682644+PacManGod@users.noreply.github.com>
2023-01-17 10:39:52 -0500

    Update database_conexion.php

--- serviciosweb/backend/database_conexion.php
+++ serviciosweb/backend/database_conexion.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $dbname = "sweb";
 $user = "postgres";
-$password = "SupremoMLG_512";
+$password = "S----";
 $port = "5432";
 
 $conexion = pg_connect("host=$host dbname=$dbname user=$user password=$password port=$port");
